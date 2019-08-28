ee274f49aa17779144311083f252fd760dfb1261
Artyom Kazak <artyom.kazak@juspay.in>
2019-08-28 20:07:30 +0300

    Add a comment about stm-containers

--- lib/Fencer/AppState.hs
+++ lib/Fencer/AppState.hs
@@ -16,6 +16,15 @@ import Fencer.Settings
 import Fencer.Counter
 
 -- | Fencer runtime context and in-memory state.
+--
+-- A note on @stm-containers@ here: we use STM for 'appStateCounters'
+-- because updating a counter ('Fencer.Counter.updateCounter') has to be an
+-- atomic operation. If we have two parallel requests that both create a new
+-- counter, we might end up with two counters. So at the very least we want
+-- a @HashMap@ in a @TVar@, but "StmContainers.Map@ provides a better map -
+-- one where parallel queries can create entries without write-locking the
+-- entire map. See <http://nikita-volkov.github.io/stm-containers/> for more
+-- details.
 data AppState = AppState
     { appStateSettings :: !Settings
     , appStateRules :: !(StmMap.Map DomainId RuleTree)
