f37d54fd51352119a0a27a9fde881379f747ee60
Marko Dimjašević <marko.d@juspay.in>
2019-09-23 17:26:35 +0200

    Adds tasty-discover

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,15 +1,1 @@
-module Main where
-
-import qualified Fencer.Types.Test as T
-import           Test.Tasty
-
-
-tests :: TestTree
-tests = testGroup "Configuration Parsing"
-  [ T.test_parseJSONDescriptorDefinition
-  , T.test_parseJSONDomainDefinition ]
-
-
--- | Test entry point
-main :: IO ()
-main = defaultMain tests
+{-# OPTIONS_GHC -F -pgmF tasty-discover #-}
