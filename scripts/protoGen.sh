0b85c3b6515cd88116477d0337b337121bc5d58e
Marko Dimjašević <marko.d@juspay.in>
2019-10-23 12:38:01 +0200

    Revert "Issue #58: an intentional wrong test in the CI code gen stage"
    This reverts commit 5701d79de329606eb7e01ae249911b329f6e8a95.

--- scripts/protoGen.sh
+++ scripts/protoGen.sh
@@ -8,6 +8,4 @@ sed -i 's/module Rls/module Fencer.Proto/' lib/Fencer/Proto.hs
 sed -i 's/Rls\./Fencer\.Proto\./g' lib/Fencer/Proto.hs
 sed -i '1s/^/{- HLINT ignore -}\n\n/' lib/Fencer/Proto.hs
 DIFF=$(git diff lib/Fencer/Proto.hs | wc -c)
-# An intentional incorrect test to provoke a CI failure to make sure
-# this is working
-if [ "$DIFF" != "1" ]; then exit 1; fi
+if [ "$DIFF" != "0" ]; then exit 1; fi
