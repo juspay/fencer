a011ff64088cfe5f72dff617346a8f6ac04225b0
Marko Dimjašević <marko.d@juspay.in>
2019-10-23 12:13:11 +0200

    Issue #58: an intentional wrong test in the CI code gen stage

--- scripts/protoGen.sh
+++ scripts/protoGen.sh
@@ -8,4 +8,6 @@ sed -i 's/module Rls/module Fencer.Proto/' lib/Fencer/Proto.hs
 sed -i 's/Rls\./Fencer\.Proto\./g' lib/Fencer/Proto.hs
 sed -i '1s/^/{- HLINT ignore -}\n\n/' lib/Fencer/Proto.hs
 DIFF=$(git diff lib/Fencer/Proto.hs | wc -c)
-if [ "$DIFF" != "0" ]; then exit 1; fi
+# An intentional incorrect test to provoke a CI failure to make sure
+# this is working
+if [ "$DIFF" != "1" ]; then exit 1; fi
