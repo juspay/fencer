0706f787fb72c2a510e9a51cce44487daf23176b
Marko Dimjašević <marko.d@juspay.in>
2019-10-25 09:06:04 +0200

    Rework counter logic test for rule reloading (#69)
    * Use the server's getLimit instead of doing a custom lookup
    * Move and rename shouldRateLimitDescriptor to Fencer.AppState
    * Rename the Fencer.AppState module to Fencer.Logic
    * Use updateLimitCounter instead of recordHits
    * Move a counter logic test into a separate module
    
    Co-Authored-By: Artyom Kazak <artyom.kazak@juspay.in>

--- lib/Fencer/Logic.hs
+++ lib/Fencer/Logic.hs
@@ -2,20 +2,18 @@
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE RecordWildCards #-}
 
--- | In-memory state of Fencer.
-module Fencer.AppState
+-- | In-memory state of Fencer and the logic for managing it.
+module Fencer.Logic
     ( AppState
-    , appStateCounters
-    , appStateRules
     , initAppState
 
     -- * Methods for working with 'AppState'
-    , recordHits
     , getLimit
     , setRules
     , getAppStateRulesLoaded
     , updateCurrentTime
     , deleteCountersWithExpiry
+    , updateLimitCounter
     )
 where
 
@@ -154,6 +152,31 @@ getLimit appState domain descriptor =
     StmMap.lookup domain (appStateRules appState) >>= \case
         Nothing -> pure Nothing
         Just ruleTree -> pure (applyRules descriptor ruleTree)
+
+-- | Handle a single descriptor in a 'shouldRateLimit' request.
+--
+-- Returns the current limit and response.
+--
+-- 'updateLimitCounter' will create a new counter if the counter does
+-- not exist, or update an existing counter otherwise. The counter will be
+-- reset if it has expired, and 'appStateCounterExpiry' will be updated.
+updateLimitCounter
+    :: AppState
+    -> "hits" :! Word
+    -> DomainId
+    -> [(RuleKey, RuleValue)]
+    -> STM (Maybe (RateLimit, CounterStatus))
+updateLimitCounter appState (arg #hits -> hits) domain descriptor =
+    getLimit appState domain descriptor >>= \case
+        Nothing -> pure Nothing
+        Just limit -> do
+            let counterKey :: CounterKey
+                counterKey = CounterKey
+                  { counterKeyDomain = domain
+                  , counterKeyDescriptor = descriptor
+                  , counterKeyUnit = rateLimitUnit limit }
+            status <- recordHits appState (#hits hits) (#limit limit) counterKey
+            pure (Just (limit, status))
 
 -- | Set 'appStateRules' and 'appStateRulesLoaded'.
 --
---
+++ test/Fencer/Logic/Test.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Tests for "Fencer.Logic".
+module Fencer.Logic.Test
+  ( test_logicLimitUnitChange
+  ) where
+
+import           BasePrelude
+
+import           Data.List.NonEmpty (NonEmpty((:|)))
+import qualified Data.List.NonEmpty as NE
+import           Test.Tasty (TestTree)
+import           Test.Tasty.HUnit (assertEqual, testCase)
+
+import           Fencer.Counter (CounterStatus, counterRemainingLimit)
+import           Fencer.Logic (AppState, setRules, updateLimitCounter)
+import           Fencer.Rules (definitionsToRuleTree)
+import           Fencer.Server.Test (withServer, serverAppState)
+import           Fencer.Types
+
+
+-- | Test that a rule limit unit change adds a new counter and leaves
+-- the old one intact.
+test_logicLimitUnitChange :: TestTree
+test_logicLimitUnitChange =
+  withServer $ \serverIO ->
+    testCase "A rule limit unit change on rule reloading" $ do
+      state <- serverAppState <$> serverIO
+      void $ atomically $ setRules state (mapRuleDefs definitions1)
+
+      -- Record a hit and get the remaining limit
+      st1 <- getRemainingLimit <$> makeAHit state
+      assertEqual
+        "The remaining rate limit was not updated!"
+        (limit - hits)
+        st1
+
+      -- Set the new rules and the rules reloaded flag
+      atomically $ setRules state (mapRuleDefs definitions2)
+      -- Record a hit and get a remaining limit
+      st2 <- getRemainingLimit <$> makeAHit state
+      assertEqual
+        "The remaining rate was affected by a different counter!"
+        (limit - hits)
+        st2
+
+      -- Set the old rules again
+      void $ atomically $ setRules state (mapRuleDefs definitions1)
+      -- Record a hit and get a remaining limit
+      st1' <- getRemainingLimit <$> makeAHit state
+      assertEqual
+        "The old counter did not persist!"
+        (st1 - hits)
+        st1'
+ where
+  getRemainingLimit :: Maybe (RateLimit, CounterStatus) -> Word
+  getRemainingLimit = counterRemainingLimit . snd . fromMaybe (error "")
+
+  makeAHit :: AppState -> IO (Maybe (RateLimit, CounterStatus))
+  makeAHit st = atomically $
+    updateLimitCounter st (#hits hits) domainId ruleList
+
+  mapRuleDefs :: [DomainDefinition] -> [(DomainId, RuleTree)]
+  mapRuleDefs defs =
+    [ ( domainDefinitionId rule
+      , definitionsToRuleTree (NE.toList . domainDefinitionDescriptors $ rule))
+    | rule <- defs
+    ]
+
+  -- rate limit and hits in the test
+  limit = 4 :: Word
+  hits  = 1 :: Word
+
+  ruleKey   = RuleKey   "generic_key"
+  ruleValue = RuleValue "dream11_order_create"
+  ruleList  = [(ruleKey, ruleValue)]
+  domainId  = DomainId  "merchant_rate_limits"
+
+  descriptor :: DescriptorDefinition
+  descriptor = DescriptorDefinition
+        { descriptorDefinitionKey         = ruleKey
+        , descriptorDefinitionValue       = Just ruleValue
+        , descriptorDefinitionRateLimit   = Just $ RateLimit Minute limit
+        , descriptorDefinitionDescriptors = Nothing
+        }
+
+  definition1 :: DomainDefinition
+  definition1 = DomainDefinition
+    { domainDefinitionId          = domainId
+    , domainDefinitionDescriptors = descriptor :| []
+    }
+
+  definitions1 :: [DomainDefinition]
+  definitions1 = [definition1]
+
+  definition2 = definition1 {
+    domainDefinitionDescriptors =
+      (descriptor
+        { descriptorDefinitionRateLimit = Just $ RateLimit Hour limit }
+      ) :| []
+    }
+
+  definitions2 :: [DomainDefinition]
+  definitions2 = [definition2]
