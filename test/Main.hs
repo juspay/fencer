24da7e3bf38312026e9e2e427af7b139db81512c
Marko Dimjašević <marko.d@juspay.in>
2019-11-08 15:44:36 +0100

    Issue #88: fix unrelated documentation issues for a test

--- test/Main.hs
+++ test/Main.hs
@@ -16,11 +16,11 @@ tests = testGroup "All tests"
   [ Fencer.Types.Test.tests
   , Fencer.Logic.Test.tests
   , Fencer.Rules.Test.tests
-  -- 'after' is needed to avoid running the 'rules' and 'server' tests
+  -- 'after' is needed to avoid running the 'logic' and 'server' tests
   -- concurrently. Running them concurrently is problematic because
   -- both create a server (binding the same port) so if they create it
   -- at the same time, one of the test groups will fail. The 'after'
-  -- function makes 'server' tests run after 'rules' tests.
+  -- function makes 'server' tests run after 'logic' tests.
   , after AllFinish "test_logic" Fencer.Server.Test.tests
   ]
 
