f4395cd0527e07ec1ec44427ad6552e492e9711d
Artyom Kazak <artyom.kazak@juspay.in>
2019-11-08 12:38:09 +0300

    [no logic changes] Create helpers for Fencer.Server.Test

--- test/Fencer/Server/Test.hs
+++ test/Fencer/Server/Test.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE GADTs             #-}
@@ -13,10 +14,11 @@ where
 import           BasePrelude
 
 import           Test.Tasty (TestTree, testGroup, withResource)
-import           Test.Tasty.HUnit (assertEqual, assertFailure, testCase)
+import           Test.Tasty.HUnit (assertEqual, assertFailure, testCase, Assertion)
 import qualified System.Logger as Logger
 import qualified System.IO.Temp as Temp
 import qualified Network.GRPC.HighLevel.Generated as Grpc
+import           Data.ByteString (ByteString)
 
 import           Fencer.Logic
 import           Fencer.Server
@@ -40,37 +42,15 @@ test_serverResponseNoRules :: TestTree
 test_serverResponseNoRules =
   withResource createServer destroyServer $ \_ ->
     testCase "When no rules have been loaded, all requests error out" $ do
-      -- NOTE(md): For reasons unkown, the delay in the thread makes a
-      -- server test failure for 'test_serverResponseNoRules' go
-      -- away. The delay was introduced by assuming it might help
-      -- based on the issue comment in gRPC's source code repository:
-      -- https://github.com/grpc/grpc/issues/14088#issuecomment-365852100
-      --
-      -- The length of the delay was fine tuned based on feedback from
-      -- test execution.
-      --
-      -- This delay interacts with the order of the rules and server
-      -- tests, which would have executed concurrently by default if
-      -- 'after' wasn't used in the Main test module.
-      threadDelay 5000 -- 5 ms
-
       Grpc.withGRPCClient clientConfig $ \grpcClient -> do
         client <- Proto.rateLimitServiceClient grpcClient
         response <-
           Proto.rateLimitServiceShouldRateLimit client $
             Grpc.ClientNormalRequest request 1 mempty
-        case response of
-          Grpc.ClientErrorResponse actualError ->
-            assertEqual "Got wrong gRPC error response"
-              expectedError
-              actualError
-          Grpc.ClientNormalResponse result _ _ status _ ->
-            assertFailure $
-              "Expected an error response, got a normal response: " ++
-              "status = " ++ show status ++ ", " ++
-              "result = " ++ show result
+        expectError
+          (unknownError "rate limit descriptor list must not be empty")
+          response
   where
-    -- Sample request.
     request :: Proto.RateLimitRequest
     request = Proto.RateLimitRequest
       { Proto.rateLimitRequestDomain = "domain"
@@ -78,14 +58,45 @@ test_serverResponseNoRules =
       , Proto.rateLimitRequestHitsAddend = 0
       }
 
-    -- The exact error lyft/ratelimit returns when no rules have been loaded.
-    expectedError :: Grpc.ClientError
-    expectedError =
-      Grpc.ClientIOError
-        (Grpc.GRPCIOBadStatusCode
-           Grpc.StatusUnknown
-           (Grpc.StatusDetails
-              "rate limit descriptor list must not be empty"))
+----------------------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------------------
+
+-- | Assert that a gRPC request is successful and has a specific result and
+-- status code.
+expectSuccess
+  :: (Eq result, Show result)
+  => (result, Grpc.StatusCode)
+  -> Grpc.ClientResult 'Grpc.Normal result
+  -> Assertion
+expectSuccess expected actual = case actual of
+  Grpc.ClientErrorResponse actualError ->
+    assertFailure $
+      "Expected a normal response, got an error response: " ++
+      show actualError
+  Grpc.ClientNormalResponse result _ _ status _ ->
+    assertEqual "Got wrong response" expected (result, status)
+
+-- | Assert that a gRPC request is unsuccessful.
+expectError
+  :: Show result
+  => Grpc.ClientError
+  -> Grpc.ClientResult 'Grpc.Normal result
+  -> Assertion
+expectError expected actual = case actual of
+  Grpc.ClientErrorResponse actualError ->
+    assertEqual "Got wrong gRPC error response" expected actualError
+  Grpc.ClientNormalResponse result _ _ status _ ->
+    assertFailure $
+      "Expected an error response, got a normal response: " ++
+      "status = " ++ show status ++ ", " ++
+      "result = " ++ show result
+
+-- | A constructor for the specific flavor of gRPC errors returned by Fencer.
+unknownError :: ByteString -> Grpc.ClientError
+unknownError err =
+  Grpc.ClientIOError
+    (Grpc.GRPCIOBadStatusCode Grpc.StatusUnknown (Grpc.StatusDetails err))
 
 ----------------------------------------------------------------------------
 -- gRPC server
@@ -116,6 +127,21 @@ createServer = do
   serverLogger   <- getLogLevel >>= newLogger (Logger.Path loggerPath)
   serverAppState <- initAppState
   serverThreadId <- forkIO $ runServer serverLogger serverAppState
+
+  -- NOTE(md): For reasons unkown, the delay in the thread makes a
+  -- server test failure for 'test_serverResponseNoRules' go
+  -- away. The delay was introduced by assuming it might help
+  -- based on the issue comment in gRPC's source code repository:
+  -- https://github.com/grpc/grpc/issues/14088#issuecomment-365852100
+  --
+  -- The length of the delay was fine tuned based on feedback from
+  -- test execution.
+  --
+  -- This delay interacts with the order of the rules and server
+  -- tests, which would have executed concurrently by default if
+  -- 'after' wasn't used in the Main test module.
+  threadDelay 5000 -- 5 ms
+
   pure Server{..}
 
 -- | Kill Fencer.
