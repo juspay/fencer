cfa714f3908ec50ad6198676eea9c1a36a03b824
Marko Dimjašević <marko.d@juspay.in>
2019-11-26 18:11:07 +0100

    Issue #26: Add a server test for the case when no configuration is found (#106)
    * Introduce a server test for the case when there is no configuration

--- test/Fencer/Server/Test.hs
+++ test/Fencer/Server/Test.hs
@@ -14,6 +14,7 @@ where
 
 import           BasePrelude
 
+import           Control.Monad (replicateM_)
 import           Data.ByteString (ByteString)
 import qualified Data.Vector as Vector
 import           GHC.Exts (fromList)
@@ -50,7 +51,8 @@ import qualified Fencer.Proto as Proto
 
 tests :: TestTree
 tests = testGroup "Server tests"
-  [ test_serverResponseNoRules
+  [ test_serverResponseRulesNotLoaded
+  , test_serverResponseNoRules
   , test_serverResponseEmptyDomain
   , test_serverResponseEmptyDescriptorList
   , test_serverResponseReadPermissions
@@ -58,18 +60,19 @@ tests = testGroup "Server tests"
   , test_serverResponseDuplicateRule
   ]
 
--- | Test that when Fencer is started without any rules provided to it (i.e.
--- 'reloadRules' has never been ran), requests to Fencer will error out.
+-- | Test that when Fencer is started without rule loading (i.e.
+-- 'reloadRules' has never been ran), requests to Fencer will error
+-- out.
 --
 -- This test also corresponds to a case when there is faulty
 -- configuration and Fencer loads no rules at startup. In such a case
 -- the server responds to requests with an error.
 --
 -- This behavior matches @lyft/ratelimit@.
-test_serverResponseNoRules :: TestTree
-test_serverResponseNoRules =
+test_serverResponseRulesNotLoaded :: TestTree
+test_serverResponseRulesNotLoaded =
   withResource createServer destroyServer $ \serverIO ->
-    testCase "When no rules have been loaded, all requests error out" $ do
+    testCase "When no rule loading was done, all requests error out" $ do
       server <- serverIO
       withService server $ \service -> do
         response <- Proto.rateLimitServiceShouldRateLimit service $
@@ -77,6 +80,39 @@ test_serverResponseNoRules =
         expectError
           (unknownError "no rate limit configuration loaded")
           response
+  where
+    request :: Proto.RateLimitRequest
+    request = Proto.RateLimitRequest
+      { Proto.rateLimitRequestDomain = "domain"
+      , Proto.rateLimitRequestDescriptors =
+          fromList $
+          [ Proto.RateLimitDescriptor $
+              fromList [Proto.RateLimitDescriptor_Entry "key" "value"]
+          ]
+      , Proto.rateLimitRequestHitsAddend = 0
+      }
+
+-- | Test that when Fencer is started and there are no rules after
+-- loading (i.e., 'reloadRules' has been ran, but there is no
+-- configuration), requests to Fencer will be responded to with OK.
+--
+-- This behavior matches @lyft/ratelimit@.
+test_serverResponseNoRules :: TestTree
+test_serverResponseNoRules =
+  withResource createServer destroyServer $ \serverIO ->
+    testCase "When no rules have been loaded, all responses are OK" $ do
+      server <- serverIO
+      atomically (setRules (serverAppState server) []) -- an empty rule list
+      withService server $ \service -> do
+        let communicationRoundtrip = do
+              response <- Proto.rateLimitServiceShouldRateLimit service $
+                Grpc.ClientNormalRequest request 1 mempty
+              expectSuccess
+                (genericOKResponse, Grpc.StatusOk)
+                response
+        -- Test that having a sequence of requests does not change the
+        -- server state
+        replicateM_ 5 communicationRoundtrip
   where
     request :: Proto.RateLimitRequest
     request = Proto.RateLimitRequest
@@ -338,7 +374,7 @@ createServer = do
   serverThreadId <- forkIO $ runServerWithPort serverPort serverLogger serverAppState
 
   -- NOTE(md): For reasons unkown, without a delay the delay in the thread makes a
-  -- server test failure for 'test_serverResponseNoRules' go
+  -- server test failure for 'test_serverResponseRulesNotLoaded' go
   -- away. See <https://github.com/juspay/fencer/issues/53>.
   --
   -- The delay was introduced by assuming it might help
